074a3a989732bcd9679bfb5d77a46e23a4304659
CoryNoonan <cory@maxwellhealth.com>
2016-11-26 18:43:13 -0500

    if no link dont show image

--- src/pages/Projects/Project.tsx
+++ src/pages/Projects/Project.tsx
@@ -8,17 +8,13 @@ export default function Project(props: ProjectProps): JSX.Element {
   return (
     <div className="project" key={title}>
       <div className="project-image-container">
-        {_.map(imageLinks, (imageLink: string) => <a href={link}><img key={imageLink} className="project-image" src={imageLink || ''} /></a> )}
+        {_.map(imageLinks, (imageLink: string) => <a target="_blank" href={link}><img key={imageLink} className="project-image" src={imageLink || ''} /></a> )}
       </div>
       <div className="project-info-container">
         <h1 className="project-title">{title || ''}</h1>
         <div className="project-link-container">
-          <a target="_blank" className="project-link" href={github || ''}>
-            <i className="fa fa-github" aria-hidden="true" />
-          </a>
-          <a target="_blank" className="project-link" href={link || ''}>
-            <i className="fa fa-link" aria-hidden="true" />
-          </a>
+          <a target="_blank" className="project-link" href={github || ''}><i className="fa fa-github" aria-hidden="true" /></a>
+          {link ? <a target="_blank" className="project-link" href={link || ''}><i className="fa fa-link" aria-hidden="true" /></a> : null}
         </div>
         <h3 className="project-details">{details || ''}</h3>
         <div className="project-description">{(description || '').split('\n').map((text: string) => <p className="project-description-paragraph" key={text}>{text}<br /></p>)}</div>
