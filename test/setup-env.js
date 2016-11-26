2a54071404f78d3afaa9c221b0b1b00de906657a
CoryNoonan <cory@maxwellhealth.com>
2016-11-26 18:51:19 -0500

    update test setup

--- test/setup-env.js
+++ test/setup-env.js
@@ -1,20 +1,7 @@
 const _ = require('lodash')
-const jsdom = require('jsdom')
-const ReactDOM = require('react-dom')
-require('babel-polyfill')
-require('ts-node').register({ disableWarnings: false })
+const { jsdom } = require('jsdom')
+require('ts-node').register()
 
-const mockDocument = '<!doctype html><html><body><div id="app"></div></body></html>'
-
-global['document'] = jsdom.jsdom(mockDocument);
-global['window'] = global['window'] || {};
+global['document'] = jsdom('<!doctype html><html><body><div id="app"></div></body></html>');
 global['window'] = _.extend(global['window'], document.defaultView);
 global['navigator'] = { userAgent: 'node.js' };
-
-
-if (typeof afterEach !== 'undefined') {
-  afterEach(() => {
-    ReactDOM.unmountComponentAtNode(document.getElementById('app'))
-  });
-};
-
