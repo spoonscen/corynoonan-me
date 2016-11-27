2199114f8c1310aee69d2d929879aeeeff9e63f3
CoryNoonan <cory@maxwellhealth.com>
2016-11-27 09:52:56 -0500

    move root app to the index file

--- src/index.spec.tsx
+++ src/index.spec.tsx
@@ -1,18 +1,30 @@
+import * as React from 'react'
 import { expect } from 'chai'
 import * as enzyme from 'enzyme'
-import { IndexPage } from './index'
-import { Router, Route } from 'react-router'
+import { IndexPage, RootApp, RootAppProps } from './index'
+import { Router, Route, Link } from 'react-router'
 
 
-describe('index.tsx', () => {
+describe('index', () => {
   let wrapper: enzyme.ShallowWrapper<any, any>
-  beforeEach(() =>
-    wrapper = enzyme.shallow(IndexPage)
-  )
 
   it('has a router and routes', () => {
+    wrapper = enzyme.shallow(IndexPage)
     expect(wrapper.find(Router)).to.have.length(1)
     expect(wrapper.find(Route)).to.have.length(4)
   })
 
+  it('has the RootApp component', () => {
+    const rootAppProps: RootAppProps = {
+      children: [IndexPage]
+    }
+    wrapper = enzyme.shallow(<RootApp {...rootAppProps}/>)
+    expect(wrapper.find('.root-app')).to.have.length(1)
+    const links = wrapper.find(Link)
+    expect(links).to.have.length(4)
+    expect(links.find({to: 'main'})).to.have.length(1)
+    expect(links.find({to: 'projects'})).to.have.length(1)
+    expect(links.find({to: 'blog'})).to.have.length(1)
+    expect(links.find({to: 'about'})).to.have.length(1)
+  })
 })
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react'
 import { render } from 'react-dom'
-import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import { Router, Route, IndexRoute, hashHistory, Link } from 'react-router'
 import { BlogPage } from './pages/BlogPage'
 import { MainPage } from './pages/MainPage'
-import { App } from './components/App'
 import ProjectsPageContainer from './pages/Projects/ProjectsPageContainer'
 import AboutPageContainer from './pages/About/AboutPageContainer'
 import './styles/app.scss'
@@ -11,10 +10,26 @@ import '../public/assets/vendor/font-awesome-4.7.0/css/font-awesome.min.css'
 
 const app = document.getElementById('app')
 
+export interface RootAppProps {
+  children: JSX.Element[]
+}
+
+export function RootApp(props: RootAppProps): JSX.Element {
+    return (
+      <div className="root-app">
+        <Link key="main" to="main" />
+        <Link key="projects" to="projects" />
+        <Link key="blog" to="blog" />
+        <Link key="about" to="about" />
+        {props.children}
+      </div>
+    )
+}
+
 export const IndexPage = (
   <div className="index-container">
     <Router history={hashHistory}>
-      <Route path="/" component={App}>
+      <Route path="/" component={RootApp}>
         <IndexRoute component={MainPage} />
         <Route path="blog" component={BlogPage} />
         <Route path="projects" component={ProjectsPageContainer} />
