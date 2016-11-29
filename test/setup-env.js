b8f5b5fd8e048d8bcc99bb48c1ad583feb13b189
CoryNoonan <cory@maxwellhealth.com>
2016-11-28 22:35:37 -0500

    set up code coverage reports

--- test/setup-env.js
+++ test/setup-env.js
@@ -1,6 +1,6 @@
 const _ = require('lodash')
 const { jsdom } = require('jsdom')
-require('ts-node').register()
+// require('ts-node').register()
 
 global['document'] = jsdom('<!doctype html><html><body><div id="app"></div></body></html>');
 global['window'] = _.extend(global['window'], document.defaultView);
