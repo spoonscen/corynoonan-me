ad4d3e16c630a0724b9b29e56bbdefb35c415b58
CoryNoonan <cory@maxwellhealth.com>
2016-11-28 22:38:42 -0500

    remove ts-node from test env require stuff

--- test/setup-env.js
+++ test/setup-env.js
@@ -1,6 +1,5 @@
 const _ = require('lodash')
 const { jsdom } = require('jsdom')
-// require('ts-node').register()
 
 global['document'] = jsdom('<!doctype html><html><body><div id="app"></div></body></html>');
 global['window'] = _.extend(global['window'], document.defaultView);
