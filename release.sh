10b02e0725577840ee457baf09c6ed89d2a4b76b
Cory <cory.noonan@gmail.com>
2016-12-15 22:00:40 -0500

    release script

---
+++ release.sh
@@ -0,0 +1,18 @@
+git checkout -B release
+echo 'On Release Branch!'
+cp .gitignore .gitignore.master
+echo 'Backed up .gitignore'
+cp .gitignore.release .gitignore
+echo 'Implemented release .gitignore'
+webpack
+echo 'Bundled to dist!'
+git add .
+git commit -m 'release build'
+git push origin release -f
+echo 'Force Pushed Release!'
+cp .gitignore.master .gitignore
+echo 'replace .gitignore'
+rm -f .gitignore.master
+echo 'delete .gitignore backup'
+echo 'Released!'
+git checkout master
